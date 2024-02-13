686d169d8105c8236cd790f394fe80afd8060cc6
Peadar Grant <grantp@dkit.ie>
2024-02-13 13:03:40 +0000

    tidy-up from class

--- t04_dynamodb/dynamodb_commands.ps1
+++ t04_dynamodb/dynamodb_commands.ps1
@@ -1,15 +1,26 @@
 # Commands for DynamoDb
+# Peadar Grant
 
-# show all tables in our account
+# show all tables in AWS account
 aws dynamodb list-tables
 
 # entire contents of a table
 aws dynamodb scan --table-name tasks
 
-# capture contents of table to JSON
+# capture entire table items to JSON
 $Tasks = (aws dynamodb scan --table-name tasks | ConvertFrom-Json).Items
 
 # pick out 2nd item, name attribute of type string (S)
 $Tasks[1].name.S
+               # S: String
+			   # N: Numeric
+			   # BOOL: boolean
 
+# loop over table
+foreach ( $item in $Items ) {
+	# work with each item here.
+}
+			   
+# delete table
+aws dynamodb delete-table --table-name $TableName
 
